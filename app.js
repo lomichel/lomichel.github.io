df6f3ea7f23237c4717291ab5f153231c39c8340
lomichel <lomichel23@gmail.com>
2016-08-12 18:05:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,26 @@ var main = function() {
     });
 });
 
+function cycleBackgrounds() {
+	var index = 0;
 
+	$imageEls = $('.toggle-image'); // Get the images to be cycled.
+
+	setInterval(function () {
+		// Get the next index.  If at end, restart to the beginning.
+		index = index + 1 < $imageEls.length ? index + 1 : 0;
+		// Show the next image.
+		$imageEls.eq(index).addClass('show');
+		// Hide the previous image.
+		$imageEls.eq(index - 1).removeClass('show');
+
+	}, 4000);
 };
-$(document).ready(main);
 
+// Document Ready.
+$(function () {
+	cycleBackgrounds();
+});
+	
+	};
+$(document).ready(main);
