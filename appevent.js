2c195d7c0a1d8999545d7f348a2eb3ac9ac1e88a
lomichel <lomichel23@gmail.com>
2016-08-26 15:38:53 +0200

    Update appevent.js

--- appevent.js
+++ appevent.js
@@ -1,3 +1,50 @@
+
+     var popup = (function() 
+{
+ 
+    function init() {
+ 
+        var overlay = $('.overlay');
+ 
+        $('.popup-button').each(function(i, el)
+        {
+            var modal = $('#' + $(el).attr('data-modal'));
+            var close = $('.close');
+ 
+            // fonction qui enleve la class .show de la popup et la fait disparaitre
+            function removeModal() {
+                modal.removeClass('show');
+            }
+ 
+            // evenement qui appelle la fonction removeModal()
+            function removeModalHandler() {
+                removeModal(); 
+            }
+ 
+            // au clic sur le bouton on ajoute la class .show a la div de la popup qui permet au CSS3 de prendre le relai
+            $(el).click(function()
+            {   
+                modal.addClass('show');
+                overlay.unbind("click");
+                // on ajoute sur l'overlay la fonction qui permet de fermer la popup
+                overlay.bind("click", removeModalHandler);
+            });
+ 
+            // en cliquant sur le bouton close on ferme tout et on arrête les fonctions
+            close.click(function(event)
+            {
+                event.stopPropagation();
+                removeModalHandler();
+            });
+ 
+        });
+    }
+ 
+    init();
+ 
+})();
+
+
 var main = function() {
   /* Push the body and the nav over by 285px over */
   $('.icon-menu').click(function() {
@@ -70,50 +117,6 @@ $(document).ready(function()
          }, 1000);
      });
      
-     var popup = (function() 
-{
- 
-    function init() {
- 
-        var overlay = $('.overlay');
- 
-        $('.popup-button').each(function(i, el)
-        {
-            var modal = $('#' + $(el).attr('data-modal'));
-            var close = $('.close');
- 
-            // fonction qui enleve la class .show de la popup et la fait disparaitre
-            function removeModal() {
-                modal.removeClass('show');
-            }
- 
-            // evenement qui appelle la fonction removeModal()
-            function removeModalHandler() {
-                removeModal(); 
-            }
- 
-            // au clic sur le bouton on ajoute la class .show a la div de la popup qui permet au CSS3 de prendre le relai
-            $(el).click(function()
-            {   
-                modal.addClass('show');
-                overlay.unbind("click");
-                // on ajoute sur l'overlay la fonction qui permet de fermer la popup
-                overlay.bind("click", removeModalHandler);
-            });
- 
-            // en cliquant sur le bouton close on ferme tout et on arrête les fonctions
-            close.click(function(event)
-            {
-                event.stopPropagation();
-                removeModalHandler();
-            });
- 
-        });
-    }
- 
-    init();
- 
-})();
 $(document).ready(popup);
 $(document).ready(main);
 
