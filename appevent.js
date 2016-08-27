c3bc9b055ab191e112d695b0885eea5306c3de71
lomichel <lomichel23@gmail.com>
2016-08-27 14:48:35 +0200

    Update appevent.js

--- appevent.js
+++ appevent.js
@@ -1,3 +1,21 @@
+$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+ 
+        e.preventDefault();
+    });
+ 
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+ 
+        e.preventDefault();
+    });
+});
+
 
 var main = function() {
   /* Push the body and the nav over by 285px over */
