893c5f2b0a86d3afa65f3958b2d7535c3663c57c
lomichel <lomichel23@gmail.com>
2016-09-03 17:43:02 +0200

    Update appevent.js

--- appevent.js
+++ appevent.js
@@ -88,7 +88,15 @@ $(document).ready(function()
             });
          }, 1000);
      });
-     
+    
+
+$(document).ready(function(){
+    $(".flip").flip({
+        trigger: 'hover'
+    });
+});
+
+ 
 $(document).ready(main);
 
 
