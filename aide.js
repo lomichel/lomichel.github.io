67dc27a5d146ca2be22c8ba49e9a4e67c21a15f6
lomichel <lomichel23@gmail.com>
2016-11-19 17:27:08 +0100

    Update aide.js

--- aide.js
+++ aide.js
@@ -1,22 +1,3 @@
-$(function() {
-    //----- OPEN
-    $('[data-popup-open]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-open');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
- 
-        e.preventDefault();
-    });
- 
-    //----- CLOSE
-    $('[data-popup-close]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-close');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
- 
-        e.preventDefault();
-    });
-});
-
-
 var main = function() {
   /* Push the body and the nav over by 285px over */
   $('.icon-menu').click(function() {
@@ -36,84 +17,31 @@ var main = function() {
     }, 200);
 
     $('body').animate({
-      left: "10%"
+      left: "0px"
     }, 200);
   });
-
 }; 
 
-$(document).ready(function () {
-    $("#outer1 img").click(function () {
-        var th = $(this);
-          var text1 =document.querySelector("#text1");
-        if ($(th).css('left') == '0px') {
-            console.log("ret");
-            $(th).animate({
-                "left": "-300px"
-            }, 1500)
- text1.style["display"] = "flex";
-        }  
-        else {
-      
-            console.log("sdffsdsff");
-            $(th).animate({
-                "left": "0px"
-            }, 1500)
-           
- text1.style["display"] = "none";
-            
-        }
-   
+
+ $(document).ready(function(){
+    $(".outer1").click(function(){
+        $(".text1").toggle();
+    });
+});
+ $(document).ready(function(){
+    $(".outer2").click(function(){
+        $(".text2").toggle();
+    });
+});
+ $(document).ready(function(){
+    $(".outer3").click(function(){
+        $(".text3").toggle();
+    });
+});
+ $(document).ready(function(){
+    $(".outer4").click(function(){
+        $(".text4").toggle();
     });
 });
 
-$(document).ready(function () {
-    $("#outer2 img").click(function () {
-        var th = $(this);
-          var text2 =document.querySelector("#text2");
-        if ($(th).css('left') == '0px') {
-            console.log("ret");
-            $(th).animate({
-                "left": "-300px"
-            }, 1500)
- text2.style["display"] = "flex";
-        }  
-        else {
-      
-            console.log("sdffsdsff");
-            $(th).animate({
-                "left": "0px"
-            }, 1500)
-           
- text2.style["display"] = "none";
-            
-        }
-   
-    });
-});
-
-$(document).ready(function () {
-    $("#outer3 img").click(function () {
-        var th = $(this);
-          var text3 =document.querySelector("#text3");
-        if ($(th).css('left') == '0px') {
-            console.log("ret");
-            $(th).animate({
-                "left": "-300px"
-            }, 1500)
- text3.style["display"] = "flex";
-        }  
-        else {
-      
-            console.log("sdffsdsff");
-            $(th).animate({
-                "left": "0px"
-            }, 1500)
-           
- text3.style["display"] = "none";
-            
-        }
-   
-    });
-});
 $(document).ready(main);
