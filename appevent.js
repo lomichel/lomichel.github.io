7770339d319236fd6e83124150380763ff473876
lomichel <lomichel23@gmail.com>
2017-05-23 18:32:06 +0200

    Update appevent.js

--- appevent.js
+++ appevent.js
@@ -1,47 +1,3 @@
-$(function() {
-    //----- OPEN
-    $('[data-popup-open]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-open');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
- 
-        e.preventDefault();
-    });
- 
-    //----- CLOSE
-    $('[data-popup-close]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-close');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
- 
-        e.preventDefault();
-    });
-});
-
-
-var main = function() {
-  /* Push the body and the nav over by 285px over */
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
-
-    $('body').animate({
-      left: "285px"
-    }, 200);
-  });
-
-  /* Then push them back */
-  $('.icon-close').click(function() {
-    $('.menu').animate({
-      left: "-285px"
-    }, 200);
-
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
-}; 
-
-
 
  $(document).ready(function(){
     $("#image-5").click(function(){
@@ -108,6 +64,14 @@ $('#image-8').click(function(){
     $("html, body").animate({ scrollTop: 1820 }, 1000);
     return false;
 });
+$('#image-9').click(function(){
+    $("html, body").animate({ scrollTop: 1820 }, 1000);
+    return false;
+});
+$('#image-10').click(function(){
+    $("html, body").animate({ scrollTop: 1820 }, 1000);
+    return false;
+});
  
   $(document).ready(function(){
     $("#image-6").click(function(){
@@ -160,6 +124,7 @@ $('#image-8').click(function(){
       
     });
 });
+
 $(document).ready(main);
 
 
